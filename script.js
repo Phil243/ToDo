c866478f0b28c54028cce46e1dae1714646e6a05
Philipp Strauß <phil13.strauss@gmail.com>
2022-04-07 16:44:11 +0200

    repair merge

--- script.js
+++ script.js
@@ -4,27 +4,29 @@ const list = document.getElementById("todo-list");
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    //Li-text:
+
     const inputValue = document.getElementById("todo-input").value;
     const newListItem = document.createElement("li");
     newListItem.className = "list-group-item";
     newListItem.innerHTML = ` ${inputValue}`;
     list.append(newListItem);
-    //Checkbox:
     const newListInput = document.createElement('input');
     newListInput.className = "form-check-input me-1";
     newListInput.type = "checkbox";
     newListInput.value ="";
     newListInput.ariaLabel = "...";
     newListItem.prepend(newListInput);
-    //Delete Button:
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'Delete';
     deleteButton.className = "delete";
     newListItem.append(deleteButton);
 
+    //const deleteButton = document.querySelector('.delete');
+
     const handleDelete = (event) => {
+
         newListItem.remove();
+
     };
   
     deleteButton.addEventListener('click', handleDelete);
@@ -34,5 +36,15 @@ const handleSubmit = (event) => {
   form.addEventListener("submit", handleSubmit);
 
 
+  //Section: delete button effects
 
+const deleteButton = document.querySelector('.delete');
+
+  const handleDelete = (event) => {
+    deleteButton.innerText = "I was clicked!"
+    event.target.innerHTML = "The event target was clicked!"
+  };
+
+  //deleteButton.addEventListener('click', handleDelete);
+  //deleteButton.innerHTML = handleDelete;
 
